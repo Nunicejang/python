105b56ba2f014da56b5f20f8ee562e3500ab14b4
Watcharaporn Laochai <watcharaporn8221@gmail.com>
2022-07-05 14:23:23 +0700

    first commit

---
+++ work-1.py
@@ -0,0 +1,11 @@
+print("hello M.6/14")
+print("Netchanok Leknoy")
+print("Gam")
+print("17 years old")
+print("Chonradsadornumrung")
+print("garusxm")
+print("9 January 2005")
+print("Live in Chonburi")
+print("Anime lovers")
+print("Video created")
+#น.ส.วัชรภรณ์ เหลาชัย ม.6/14 43
