1406a4a02cc241ee27e8df6867b6c1056be0b589
Watcharaporn Laochai <watcharaporn8221@gmail.com>
2022-07-05 14:41:33 +0700

    second commit

--- profile.py
+++ profile.py
@@ -1,6 +1,6 @@
 profile = {
-    "name": "Netchanok",
-    "surName":"Leknoy",
+    "name": "Watcharaporn",
+    "surName":"Laochai",
     "age": 17,
     "hobbies": ["listen a music", "play game", "read a book"],
     "food": {"ข้าวผัด" : "7-11", "ก๋วยเตี๋ยว" : "โรงเรียนฉัน", "เทมปุระกุ้ง" : "ยาโยอิ", "โดนัท" : "Mr.Donut", "ยำไก่แซ่บ" : "KFC"},
--- work-2.py
+++ work-2.py
@@ -1,16 +1,16 @@
 M ='6/14'
 print(M)
-Myname ='Netchanok Lenoy'
+Myname ='Watcharaporn Laochai'
 print(Myname)
-Mynickname = 'Gam'
+Mynickname = 'Nice'
 print(Mynickname)
 Myold = '17 years old'
-Pseudonym = 'garusxm'
+Pseudonym = 'love cat'
 print(Pseudonym)
 print(Myold)
 Myschool = 'Chonradsadornumrung'
 print(Myschool)
-Mybirthday = '9 January 2005'
+Mybirthday = '30 August 2004'
 print(Mybirthday)
 Habitat = 'Chonburi'
 print(Habitat)
